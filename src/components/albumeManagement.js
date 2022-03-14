9c731bb56ae7aabf80f0cd2bc1ec2aa20c53a9cb
abdullah-fahad <72708185+abdullah-fahad@users.noreply.github.com>
2022-03-14 20:58:14 +0300

    Adding album management feature

---
+++ src/components/albumeManagement.js
@@ -0,0 +1,34 @@
+import React, {useState} from 'react'
+import axios from 'axios'
+
+var getAlbum = async() => {
+    var album;
+    await axios.get("https://al-faisaliah-highschool.herokuapp.com/album").then(response => album = response.data).catch(err => console.log(err))
+    return album;
+  }
+
+var AlbumeManagement = () =>{
+    var [album, setAlbum] = useState()
+    if(!album){getAlbum().then(res => setAlbum(res))}
+    if(album) {
+        return (
+            <div className='album-image-container'>
+                <img className='album-image' src="https://via.placeholder.com/150" />
+                <span className='album-caption'>Hey!</span>
+                <div className='btn-group'>
+                    <button className='btn btn-info album-btn'>تعديل الإسم</button>
+                    <button className='btn btn-danger album-btn'>حذف</button>
+                </div>
+            </div>
+        )
+    }
+    else{
+        return(
+            <h1>
+                لايوجد أي صور في الألبوم
+            </h1>
+        )
+    }
+}
+
+export default AlbumeManagement;
--- src/components/managerAlbum.js
+++ src/components/managerAlbum.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Album from './album';
+import AlbumeManagement from './AlbumeManagement';
 import './styles.css';
 
 var ManagerAlbum = () => {
@@ -10,7 +11,7 @@ var ManagerAlbum = () => {
             <button className="btn btn-success">إضافة صورة <img alt="alfaisaliah-highschool"src="https://img.icons8.com/ios/20/ffffff/plus--v2.png"/></button>
             <br />
             <br />
-            <h1><Album /></h1>
+            <h1><AlbumeManagement /></h1>
         </div>
     );
 }
